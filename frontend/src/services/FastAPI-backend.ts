0bcb783d85a89ec4ad9f57f0760f6b0aebf965db
cynic04 <firegate0@gmail.com>
2026-08-29 04:08:16 -0400

    Fixed a really silly wording error

--- frontend/src/services/FastAPI-backend.ts
+++ frontend/src/services/FastAPI-backend.ts
@@ -1,6 +1,7 @@
-const API_URL = import.meta.env.VITE_BACKEND_API_URL_FRONTEND;
+const API_URL = import.meta.env.VITE_BACKEND_API_URL_PRODUCTION;
 
 export async function getRequest() {
+    console.log('Making GET request to:', API_URL);
     try {
         const response = await fetch(`${API_URL}`, {
             method: 'GET',
