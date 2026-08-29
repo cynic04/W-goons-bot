9ec9ecfb5a916bb391a2c17c1c3cd5395961c959
cynic04 <firegate0@gmail.com>
2026-08-29 03:32:04 -0400

    Implemented Browser Router for this project so that this is a multi-page application + vercel.json added to protect against 404 errors on page refresh

---
+++ frontend/src/pages/SelectTagsPage.tsx
@@ -0,0 +1,10 @@
+function SelectTagsPage() {
+    return (
+        <div>
+            <h1>Select Tags Page</h1>
+            <p>This is the Select Tags page.</p>
+        </div>
+    );
+}
+
+export default SelectTagsPage
