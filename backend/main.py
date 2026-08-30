3b5b03ed0792093be1bfd8fea22f59d96c525375
cynic04 <firegate0@gmail.com>
2026-08-29 21:23:24 -0400

    Add API call to FastAPI endpoint that interfaces with R34. Added an API test file to the gitignore

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,10 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 from pydantic import BaseModel
+import random
+import json
+import xmltodict
+import requests
 # Define the origins that are allowed to make requests to the backend
 load_dotenv()  
 origins = [
@@ -59,5 +63,26 @@ async def add_tag(request: TagRequest):
     # If no tag is provided, return an error message
     return {
         "message": "No tag provided.",
+    }
+
+@app.get("/api/get-goons")
+async def get_goons():
+    supabase: Client = create_client(DATABASE_URL, DATABASE_KEY)
+    get_tags = supabase.table("goon_tags").select("tag_listing").eq("id", "1").execute()
+    # If data is returned from the database, get the list of tags and return a random tag to the frontend
+    if get_tags.data:
+       tags = get_tags.data[0]["tag_listing"]["tags"]
+       if tags:
+            random_tag = random.choice(tags)
+            r34_response = requests.get(os.getenv("API_LINK_POSTS_R34") + f"&limit=10&tags={random_tag}")
+            data_json = xmltodict.parse(r34_response.text)
+            return {
+                "message": "Images retrieved successfully!",
+                "data": data_json
+            }
+       
+    return {
+        "message": "No tags found in the database.",
+        "tags": []
     }
             
