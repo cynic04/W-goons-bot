46d79fcf39111f195b58ac4a1c34d25bc3717b77
cynic04 <firegate0@gmail.com>
2026-09-12 00:47:05 -0400

    backend now does not return videos and will choose 10 random posts

--- backend/main.py
+++ backend/main.py
@@ -87,7 +87,7 @@ async def get_goons():
        # If there are tags in the list, choose a random tag and make a request to the Rule34 API to get posts with that tag
        if tags:
             random_tag = random.choice(tags)
-            r34_response = requests.get(os.getenv("API_LINK_POSTS_R34") + f"&limit=10&tags={random_tag} -ai_generated")
+            r34_response = requests.get(os.getenv("API_LINK_POSTS_R34") + f"&limit=10&tags={random_tag} -ai_generated sort:random ")
             data_json = xmltodict.parse(r34_response.text)
 
             return {
