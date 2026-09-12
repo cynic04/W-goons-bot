a2bdfa41d76e44a0d116b62d5a898661fe0910df
cynic04 <firegate0@gmail.com>
2026-09-12 00:48:31 -0400

    GoonCards now uses Material UI's Card component definitions. Button hovering color changed, and style of how the cards are displayed is changed

--- frontend/src/components/GoonCards.tsx
+++ frontend/src/components/GoonCards.tsx
@@ -1,18 +1,28 @@
 import '../css/GoonCards.css';
+import {
+    Card,
+    CardActionArea,
+    CardContent,
+    Typography
+} from '@mui/material';
 
 function GoonCards({ goons }: { goons: any[] }) {
     return (
         <div className="goons-grid">
             {/* Easiest way to read this: for each goon in the returned goons array, create a figure element with an image and caption */}
             {/* "goon: any" is each individual goon in the goons array, and index is the iterator (which position in the array are we at rn) */}
-            {goons.map((goon: any, index: number) => (
-                <figure key={index} className="goon-card">
-                    <a href={goon["@file_url"]} target="_blank" rel="noopener noreferrer">
-                        <img src={goon["@file_url"]} alt={`Goon ${index + 1}`} />
-                    </a>
-                    <figcaption>Goon {index + 1}</figcaption>
-                </figure>
-            ))}
+                {goons.map((goon: any, index: number) => (
+                    <Card key={index} className="goon-card" sx={{ backgroundColor: '#313030' }}>
+                        <CardActionArea>
+                            <CardContent>
+                                <a href={goon["@file_url"]} target="_blank" rel="noopener noreferrer">
+                                    <img src={goon["@file_url"]} alt={`Goon ${index + 1}`} />
+                                </a>
+                                <Typography variant="caption" sx={{ color: 'white', fontSize: '1.5rem' }}>Goon {index + 1}</Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
         </div>
     );
 }
