3e1575d87b3bdda47c5191f4eb6f10d7c8e98c80
cynic04 <firegate0@gmail.com>
2026-09-12 01:47:22 -0400

    Adapted UI to use Material UI's definitions for grid displays. Makes the cards easier to work with

--- frontend/src/components/GoonCards.tsx
+++ frontend/src/components/GoonCards.tsx
@@ -3,26 +3,33 @@ import {
     CardActionArea,
     CardContent
 } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import '../css/GoonCards.css';
 import '../css/CenterItems.css';
 
 function GoonCards({ goons }: { goons: any[] }) {
+    // Uses Material UIs Grid and Card components to display cards in a responsive layout (xs, s and md control what happens at certain screen sizes)
+    // xs: extra small screens
+    // sm: small screens
+    // md: medium screens
     return (
-        <div className="goons-grid">
+        <Grid container spacing={2}>
             {/* Easiest way to read this: for each goon in the returned goons array, create a figure element with an image and caption */}
             {/* "goon: any[]" is each individual goon in the goons array, and index is the iterator (which position in the array are we at rn) */}
                 {goons.map((goon: any, index: number) => (
-                    <Card key={index} className="goon-card" sx={{ backgroundColor: '#313030' }}>
-                        <CardActionArea>
-                            <CardContent>
-                                <a href={goon["@file_url"]} target="_blank" rel="noopener noreferrer">
-                                    <img src={goon["@file_url"]} alt={`Goon ${index + 1}`} />
-                                </a>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
+                    <Grid key={index} size={{ xs: 6, sm: 6, md: 3 }}>
+                        <Card className="goon-card" sx={{ backgroundColor: '#313030' }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <a href={goon["@file_url"]}>
+                                        <img src={goon["@file_url"]} alt={`Goon ${index + 1}`} />
+                                    </a>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
                 ))}
-        </div>
+        </Grid>
     );
 }
 
